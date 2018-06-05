c95777a4ec1d271cafa32c07f51db1d5ffa1d2ec
Håkon <hakon.gimse@knowit.no>
2018-06-05 15:30:41 +0200

    sensor gathering

--- sensor_gathering/sensor_gathering.ino
+++ sensor_gathering/sensor_gathering.ino
@@ -10,9 +10,6 @@ int fsrAnalogPin = 1;
 int fsrReading;
 int flexPin = 2;
 int flexReading;
-int hygrometerPin = 3;
-int hygrometerReading;
-int timeBetweenReadings = 1000;
 
 void setup(){
   Serial.begin(9600);
@@ -20,18 +17,15 @@ void setup(){
 
 void loop()
 {
-  hygrometerReading = analogRead(hygrometerPin);
-  //fsrReading = analogRead(fsrAnalogPin);
-  //photocellReading = analogRead(photocellPin);
-  //flexReading = analogRead(flexPin);
-  //int chk = DHT.read11(DHT11_PIN);
+  fsrReading = analogRead(fsrAnalogPin);
+  photocellReading = analogRead(photocellPin);
+  flexReading = analogRead(flexPin);
+  int chk = DHT.read11(DHT11_PIN);
 
-  //photocellReading = map(photocellReading, 670, 1000, 0, 100);
-  //flexReading = map(flexReading, 700, 900, 0, 100);
-  hygrometerReading = constrain(hygrometerReading,400,1023);
-  hygrometerReading = map(hygrometerReading,400,1023,100,0);
+  photocellReading = map(photocellReading, 670, 1000, 0, 100);
+  flexReading = map(flexReading, 700, 900, 0, 100);
 
-  /*
+
   Serial.print(fsrReading);
   Serial.print(",");
   Serial.print(photocellReading);
@@ -41,8 +35,5 @@ void loop()
   Serial.print(DHT.temperature);
   Serial.print(",");
   Serial.println(DHT.humidity);
-  */
-  //Serial.print(",");
-  Serial.println(hygrometerReading);
-  delay(timeBetweenReadings);
+  delay(1000);
 }
