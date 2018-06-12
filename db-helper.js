5be39fbf13b9914116f17a70064735100bce87fa
andlie <anders.lien@knowit.no>
2018-06-12 11:52:38 +0200

    Think this will work

--- db-helper.js
+++ db-helper.js
@@ -18,14 +18,15 @@ client.connect(err => {
 });
 
 // eslint-disable-next-line
-async function setSensorData(pressure, light, flex, temperature, humidity) {
+//async function setSensorData(pressure, light, flex, temperature, humidity) {
+async function setSensorData(sensor_id, value) {
   const sql =
     'INSERT INTO zzzmartbed.sensor_data(sensor_id, value, datetime, user_id, bed_id)' +
-    'VALUES ()';
+    'VALUES ($1, $2, $3, $4, $5)';
 
   let results;
   try {
-    results = await client.query(sql);
+    results = await client.query(sql, [sensor_id, value, new Date(), null, "ariot_bed"]);
     client.end();
   } catch (e) {
     console.error(e.stack);
@@ -37,10 +38,10 @@ async function setSensorData(pressure, light, flex, temperature, humidity) {
 
 // eslint-disable-next-line
 async function getSensorData(days) {
-  const sql = 'SELECT * FROM zzzmartbed.sensor_data';
+  const sql = "SELECT sensor_id, AVG(value) as avg_value, date_trunc('minute', datetime) as trunc_minute FROM zzzmartbed.sensor_data WHERE datetime > current_timestamp - (? || ' day')::interval GROUP BY 1, 3 ORDER BY 3";
   let results;
   try {
-    results = await client.query(sql);
+    results = await client.query(sql, [days]);
     client.end();
   } catch (e) {
     console.error(e.stack);
